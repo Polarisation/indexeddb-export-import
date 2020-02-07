a1b7ab37b26b0f851d3976c30e9e4cd923517508
behinder <seba.szwarc at gmail.com>
2020-02-07 19:54:17 +0100

    Replacing var with const and let

--- index.js
+++ index.js
@@ -3,31 +3,41 @@
  * @param {IDBDatabase} idbDatabase - to export from
  * @param {function(Object, <string|void>)} cb - callback with signature (error, jsonString)
  */
+"use strict";
+
 function exportToJsonString(idbDatabase, cb) {
-	var exportObject = {};
-	if(idbDatabase.objectStoreNames.length === 0)
-		cb(null, JSON.stringify(exportObject));
-	else {
-		var transaction = idbDatabase.transaction(idbDatabase.objectStoreNames, "readonly");
-		transaction.onerror = function(event) {
-			cb(event, null);
-		};
-		Array.from(idbDatabase.objectStoreNames).forEach(function(storeName) {
-			var allObjects = [];
-			transaction.objectStore(storeName).openCursor().onsuccess = function(event) {
-				var cursor = event.target.result;
-				if (cursor) {
-					allObjects.push(cursor.value);
-					cursor.continue();
-				} else {
-					exportObject[storeName] = allObjects;
-					if(idbDatabase.objectStoreNames.length === Object.keys(exportObject).length) {
-						cb(null, JSON.stringify(exportObject));
-					}
-				}
-			};
-		});
-	}
+  let exportObject = {};
+  if (idbDatabase.objectStoreNames.length === 0)
+    cb(null, JSON.stringify(exportObject));
+  else {
+    let transaction = idbDatabase.transaction(
+      idbDatabase.objectStoreNames,
+      "readonly"
+    );
+    transaction.onerror = function(event) {
+      cb(event, null);
+    };
+    Array.from(idbDatabase.objectStoreNames).forEach(function(storeName) {
+      let allObjects = [];
+      transaction.objectStore(storeName).openCursor().onsuccess = function(
+        event
+      ) {
+        const cursor = event.target.result;
+        if (cursor) {
+          allObjects.push(cursor.value);
+          cursor.continue();
+        } else {
+          exportObject[storeName] = allObjects;
+          if (
+            idbDatabase.objectStoreNames.length ===
+            Object.keys(exportObject).length
+          ) {
+            cb(null, JSON.stringify(exportObject));
+          }
+        }
+      };
+    });
+  }
 }
 
 /**
@@ -39,25 +49,30 @@ function exportToJsonString(idbDatabase, cb) {
  * @param {function(Object)} cb - callback with signature (error), where error is null on success
  */
 function importFromJsonString(idbDatabase, jsonString, cb) {
-	var transaction = idbDatabase.transaction(idbDatabase.objectStoreNames, "readwrite");
-	transaction.onerror = function(event) {
-		cb(event);
-	};
-	var importObject = JSON.parse(jsonString);
-	Array.from(idbDatabase.objectStoreNames).forEach(function(storeName) {
-		var count = 0;
-		Array.from(importObject[storeName]).forEach(function(toAdd) {
-			var request = transaction.objectStore(storeName).add(toAdd);
-			request.onsuccess = function(event) {
-					count++;
-					if(count === importObject[storeName].length) { // added all objects for this store
-						delete importObject[storeName];
-						if(Object.keys(importObject).length === 0) // added all object stores
-							cb(null);
-					}
-				}
-		});
-	});
+  const transaction = idbDatabase.transaction(
+    idbDatabase.objectStoreNames,
+    "readwrite"
+  );
+  transaction.onerror = function(event) {
+    cb(event);
+  };
+  let importObject = JSON.parse(jsonString);
+  Array.from(idbDatabase.objectStoreNames).forEach(function(storeName) {
+    let count = 0;
+    Array.from(importObject[storeName]).forEach(function(toAdd) {
+      const request = transaction.objectStore(storeName).add(toAdd);
+      request.onsuccess = function() {
+        count++;
+        if (count === importObject[storeName].length) {
+          // added all objects for this store
+          delete importObject[storeName];
+          if (Object.keys(importObject).length === 0)
+            // added all object stores
+            cb(null);
+        }
+      };
+    });
+  });
 }
 
 /**
@@ -67,22 +82,26 @@ function importFromJsonString(idbDatabase, jsonString, cb) {
  * @param {function(Object)} cb - callback with signature (error), where error is null on success
  */
 function clearDatabase(idbDatabase, cb) {
-	var transaction = idbDatabase.transaction(idbDatabase.objectStoreNames, "readwrite");
-	transaction.onerror = function(event) {
-		cb(event);
-	};
-	var count = 0;
-	Array.from(idbDatabase.objectStoreNames).forEach(function(storeName) {
-		transaction.objectStore(storeName).clear().onsuccess = function() {
-			count++;
-			if(count === idbDatabase.objectStoreNames.length) // cleared all object stores
-				cb(null);
-		};
-	});
+  const transaction = idbDatabase.transaction(
+    idbDatabase.objectStoreNames,
+    "readwrite"
+  );
+  transaction.onerror = function(event) {
+    cb(event);
+  };
+  let count = 0;
+  Array.from(idbDatabase.objectStoreNames).forEach(function(storeName) {
+    transaction.objectStore(storeName).clear().onsuccess = function() {
+      count++;
+      if (count === idbDatabase.objectStoreNames.length)
+        // cleared all object stores
+        cb(null);
+    };
+  });
 }
 
 module.exports = {
-	exportToJsonString : exportToJsonString,
-	importFromJsonString : importFromJsonString,
-	clearDatabase : clearDatabase
+  exportToJsonString: exportToJsonString,
+  importFromJsonString: importFromJsonString,
+  clearDatabase: clearDatabase
 };

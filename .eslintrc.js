aa26b4aab555de4b41c73a6babeb70d22ab868a4
Justin Emery <justinemery@gmail.com>
2020-06-04 12:36:04 +0700

    Lint fixes for eslint file!

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true,
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true,
+  },
+  'extends': [
+    'eslint:recommended',
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 6,
+    'sourceType': 'module',
+    'ecmaFeatures': {
     },
-    "extends": [
-      "eslint:recommended",
-      "google"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 6,
-        "sourceType": "module",
-        "ecmaFeatures": {
-        }
-    },
-    "rules": {
-        "max-len":[2,100]
-    }
+  },
+  'rules': {
+    'max-len': [2, 100],
+  },
 };

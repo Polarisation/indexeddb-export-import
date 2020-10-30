342c01063283afd4b975f87a0bb2258f4d039fd5
Som Bhattacharyya <Som.Bhattacharyya@thoughtworks.com>
2020-10-30 15:39:45 +0530

    Use the 'clearDatabase' method to pass an optional object store argument

--- index.js
+++ index.js
@@ -185,43 +185,11 @@ function clearDatabase(idbDatabase, cb, storeName = null) {
   }
 }
 
-/**
- * Clears a chosen objectstore in a DB.
- *
- * The object store will still exist but will be empty.
- *
- * @param {IDBDatabase} idbDatabase - to delete all data from
- * @param {string} storeName - to delete all data from
- * @param {function(Object)} cb - callback with signature (error), where error is null on success
- * @return {void}
- */
-function clearObjectstore(idbDatabase, storeName, cb) {
-  const size = new Set(idbDatabase.objectStoreNames).size;
-  if (size === 0) {
-    return cb(null);
-  }
-  const transaction = idbDatabase.transaction(
-      storeName,
-      'readwrite'
-  );
-  transaction.onerror = (event) => cb(event);
-
-  let count = 0;
-  transaction.objectStore(storeName).clear().onsuccess = () => {
-    count++;
-    if (count === size) {
-      // cleared object store
-      cb(null);
-    }
-  };
-}
 if (typeof module !== 'undefined' && module.exports) {
   // We are running on Node.js so need to export the module
   module.exports = {
     exportToJsonString: exportToJsonString,
     importFromJsonString: importFromJsonString,
     clearDatabase: clearDatabase,
-    clearObjectstore: clearObjectstore,
   };
 }
-
